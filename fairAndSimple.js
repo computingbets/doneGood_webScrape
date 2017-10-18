5c9489d2b983cbbe6ae44f5d3b5d8ee40214f1c2
logicmyth <staley77@protonmail.com>
2017-10-18 13:15:27 -0400

    add vars for scrape. add data attributes for product img url

--- fairAndSimple.js
+++ fairAndSimple.js
@@ -7,7 +7,7 @@ var app     = express();
 app.get('/scrape', function(req, res){
 
   //All the web scraping magic will happen here
-  url = 'https://www.fairandsimple.com';
+  url = 'http://fairandsimple.com/shop/direct/accessories';
 
   // The structure of our request call
   // The first parameter is our URL
@@ -24,8 +24,25 @@ app.get('/scrape', function(req, res){
 
           // Finally, we'll define the variables we're going to capture
 
-          var title, release, rating;
-          var json = { title : "", release : "", rating : ""};
+          var imageURL, productURL, productName, pageTitle, productPrice, productDescription;
+          var json = { imageURL : "", productURL : "", productName : "", pageTitle : "", productPrice : "", productDescription : ""};
+
+          // We'll use the unique header class as a starting point.
+
+          $('.product-image').filter(function(){
+
+         // Let's store the data we filter into a variable so we can easily see what's going on.
+
+              var data = $(this);
+
+         // In examining the DOM we notice that the title rests within the first child element of the header tag.
+         // Utilizing jQuery we can easily navigate and get the text by writing the following code:
+
+              title = data.attr('src');
+
+         // Once we have our title, we'll store it to the our json object.
+
+              json.title = title;
       }
   })
 })
