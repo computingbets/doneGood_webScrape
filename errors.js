2ac6a4658bb23232c90aee30e380193e0d9cc9cb
logicmyth <staley77@protonmail.com>
2017-10-19 21:11:12 -0400

    create fairandsimple, starfishproject folders. add error.js

---
+++ errors.js
@@ -0,0 +1,7 @@
+//This is an error with npm itself. Please report this error at:
+//npm ERR! typeerror     <https://github.com/npm/npm/issues>
+
+//updated npm, same problem as above
+
+//i tried downloading direct from the node website but it only dls to the download directory
+//i have node 8 and npm 5 but there is something wrong with npm 
